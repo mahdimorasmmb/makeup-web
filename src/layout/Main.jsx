397940bae2d65c5c80c489f5762a82f308025e01
mahdimorasmmb <mahdimoras@gmail.com>
2021-11-28 13:13:34 +0330

    inti conifig

---
+++ src/layout/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <p>Main</p>
+      </div>
+    </>
+  );
+};
+
+export default Main;
